a667a57d2b05854d368d4cdc235c1dd2ffe599ef
mdapm9di <mdapm9di@gmail.com>
2025-09-14 13:04:47 +0800

    minor change

--- backend/src/utils/file_utils.py
+++ backend/src/utils/file_utils.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 
 import os
@@ -20,29 +19,5 @@ def generate_filename(selector_type, extract_type, language):
         base_name = base_name[:100]
     
     base_name = re.sub(r'[\\/*?:"<>|]', "_", base_name)
-    
-=======
-# -*- coding: utf-8 -*-
 
-import os
-import json
-import re
-from datetime import datetime
-
-def generate_filename(selector_type, extract_type, language):
-    """Генерация имени файла на основе текущей даты и параметров парсинга"""
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    base_name = f"parsed_data_{timestamp}"
-    
-    if language != "unknown":
-        base_name += f"_{language}"
-    
-    base_name += f"_{selector_type}_{extract_type}"
-    
-    if len(base_name) > 100:
-        base_name = base_name[:100]
-    
-    base_name = re.sub(r'[\\/*?:"<>|]', "_", base_name)
-    
->>>>>>> db3846bf44b3226c4095a015d7a96d1f900c7abe
     return base_name
